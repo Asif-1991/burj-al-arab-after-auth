604b61beb2552cfba3046ae871e4d1584080efc9
Asif-91 <asifchowdhury323@gmail.com>
2020-10-02 17:23:09 +0600

    now ready to deploy

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4_x3rg2-Z7xyZblC7bXW-rC2bU7RKvfg",
+  authDomain: "burj-al-arab-after-auth-ae601.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-after-auth-ae601.firebaseio.com",
+  projectId: "burj-al-arab-after-auth-ae601",
+  storageBucket: "burj-al-arab-after-auth-ae601.appspot.com",
+  messagingSenderId: "235141513037",
+  appId: "1:235141513037:web:12a88ca14f17c62fd0c104"
+};
+export default firebaseConfig;
